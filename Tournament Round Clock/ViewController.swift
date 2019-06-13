582ee928fbd1f98b5cff73fe0217c55229ac6530
Viran Weerasekera <viran.w@icloud.com>
2019-06-13 13:50:25 +1000

    Idk various things

--- Tournament Round Clock/ViewController.swift
+++ Tournament Round Clock/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var dsLabel: UILabel!
-    
     @IBOutlet weak var daySelector: UISegmentedControl!
     @IBOutlet weak var roundSelector: UISegmentedControl!
     @IBOutlet weak var confirmButton: UIButton!
@@ -31,10 +30,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         timeNowLabel.text = ""
         roundLabel.text = ""
         initialBuild()
-        
         
         updateButton.setTitle("", for: .normal)
         
